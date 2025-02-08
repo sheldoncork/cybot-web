722821f915928d23fda86397d1c7421b19fb57f7
Sheldon <sheldonc@iastate.edu>
2025-02-08 00:48:22 -0600

    Stopped for tonight; not working need to put socket in server side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState<string>("");
+  const formRef = useRef<HTMLFormElement>(null);
 
-  const form: HTMLFormElement = document.querySelector("form")!;
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    send_command((document.querySelector("#command_box") as HTMLInputElement).value)
-  });
-  
-  const send_command = (command: string) => {
+  useEffect(() => {
+    let form: useRef = formRef.current;
+    form = document.querySelector("form");
     
-  }
+    form.addEventListener("submit", (e: Event) => {
+      e.preventDefault();
+      const command_box: HTMLInputElement = document.querySelector("#command_box")!;
+      const command: string = command_box.value;
+      socket.sendCommand(command).then((response) => {
+        setText(response);
+      });
+      command_box.innerHTML = '';
+    });
+  }, [socket])  
+
+  const scan = async () => {
+    try{
+      const response = await fetch("mock-cybot-sensor-scan.txt");
+      if (!response.ok) {
+        throw new Error("Failed to scan");
+      }
+      const text = await response.text();
+      setText(text);
+    } catch (error){
+      console.error("Failed to scan:", error);
+      return false;
+    }
+  };
 
   return (
     <>
       <div>
-        <button>Scan</button>
+        <button onClick={scan}>Scan</button>
         <form>
           <label htmlFor="command_box"></label>
           <input type="text" id="command_box" />
         </form>
       </div>
+      <h1>Output:</h1>
+      <p>{text}</p>
     </>
   );
 }
---
+++ server/CybotSocket.ts
@@ -0,0 +1,43 @@
+const HOST = "127.0.0.1";
+const PORT = 65432;
+
+class CybotSocket {
+  private connection: Deno.Conn | null = null;
+
+  // Make TCP connection
+  async connect() {
+    try {
+      this.connection = await Deno.connect({
+        port: PORT,
+        hostname: HOST,
+      });
+      return true;
+    } catch (error) {
+      console.error("Failed to connect:", error);
+      return false;
+    }
+  }
+
+  async sendCommand(command: string): Promise<string> {
+    if (!this.connection) {
+      throw new Error("Not connected");
+    }
+
+    const encoder = new TextEncoder();
+    const decoder = new TextDecoder();
+
+    const encodedCommand = encoder.encode(command);
+    await this.connection.write(encodedCommand);
+
+    const buffer = new Uint8Array(1024);
+    const bytesRead = await this.connection.read(buffer);
+    const response = decoder.decode(buffer.subarray(0, bytesRead!)); // ! to ensure not null
+    return response;
+  }
+
+  close() {
+    this.connection?.close(); // ? to check if null
+  }
+}
+
+export default CybotSocket;
